8c961b77b40c872b6fcb45cc5dfe18fdcd94e8a2
Lucas França <lucasfreitas_c5@hotmail.com>
2022-11-04 19:25:50 -0300

    Crud musculo Thymeleaf

--- src/main/java/com/gym/projetogym/controllers/MusculoThymeController.java
+++ src/main/java/com/gym/projetogym/controllers/MusculoThymeController.java
@@ -5,22 +5,60 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gym.projetogym.model.Musculo;
 import com.gym.projetogym.repository.MusculoRepository;
 
 @Controller
-@RequestMapping("/")
 public class MusculoThymeController {
 	
 	@Autowired
 	private MusculoRepository musculoRep;
 
-	@RequestMapping
+	@GetMapping("/musculos")
 	public String showMusculos(Model model) {
 		model.addAttribute("musculos", musculoRep.findAll());
 		return "index";
 	}
-}
+	
+	@GetMapping("/musculos/criar")
+	public String novoMusculo(@ModelAttribute("musculo") Musculo musculo) {
+		return "create";
+	}
+	
+	@PostMapping("/musculos/salvar")
+	public String salvarMusculo(@ModelAttribute("musculo") Musculo musculo) {
+		musculoRep.save(musculo);
+		return "redirect:/musculos";
+	}
+	
+	
+	@GetMapping("/musculos/{id}")
+	public String alterarMusculo(@PathVariable("id") Long id, Model model) {
+		Optional<Musculo> musculoOpt = musculoRep.findById(id);
+		if(musculoOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa invalida");
+		}
+		
+		model.addAttribute("musculo", musculoOpt.get());
+		return "create";
+	}
+	
+	@GetMapping("/musculos/excluir/{id}")
+	public String excluirMusculo(@PathVariable("id") Long id) {
+		Optional<Musculo> musculoOpt = musculoRep.findById(id);
+		if(musculoOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessoa invalida");
+		}
+		
+		musculoRep.delete(musculoOpt.get());
+		return "redirect:/musculos";
+	}
+	
+	
+	
+} 
